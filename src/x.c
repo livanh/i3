cde2783e5b17c60e57ff41473e67a752da4911f8
Livanh <livanh@bulletmail.org>
2016-12-31 12:44:56 +0100

    Add support for icons in titlebars

--- src/x.c
+++ src/x.c
@@ -541,6 +541,7 @@ void x_draw_decoration(Con *con) {
 
     /* 6: draw the title */
     int text_offset_y = (con->deco_rect.height - config.font.height) / 2;
+    int text_offset_x = 0;
 
     struct Window *win = con->window;
     if (win == NULL) {
@@ -569,6 +570,9 @@ void x_draw_decoration(Con *con) {
 
     if (win->name == NULL)
         goto copy_pixmaps;
+
+    if (win->icon)
+        text_offset_x = 18;
 
     int mark_width = 0;
     if (config.show_marks && !TAILQ_EMPTY(&(con->marks_head))) {
@@ -605,11 +609,29 @@ void x_draw_decoration(Con *con) {
     i3String *title = con->title_format == NULL ? win->name : con_parse_title_format(con);
     draw_util_text(title, &(parent->frame_buffer),
                    p->color->text, p->color->background,
-                   con->deco_rect.x + logical_px(2),
+                   con->deco_rect.x + text_offset_x + logical_px(2),
                    con->deco_rect.y + text_offset_y,
-                   con->deco_rect.width - mark_width - 2 * logical_px(2));
+                   con->deco_rect.width - text_offset_x - mark_width - 2 * logical_px(2));
     if (con->title_format != NULL)
         I3STRING_FREE(title);
+
+    /* Draw the icon */
+    if (win->icon) {
+        uint16_t width = 16;
+        uint16_t height = 16;
+
+        int icon_offset_y = (con->deco_rect.height - height) / 2;
+
+        draw_util_image(
+                win->icon,
+                win->icon_width,
+                win->icon_height,
+                &(parent->frame_buffer),
+                con->deco_rect.x + text_offset_x - width,
+                con->deco_rect.y + icon_offset_y,
+                width,
+                height);
+    }
 
 after_title:
     x_draw_decoration_after_title(con, p);
